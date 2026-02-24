00666db1a057e661babc5336eba4ec96119e3750
Benedict Ogbogu <btope0132@gmail.com>
2026-02-24 03:04:18 +0100

    updated styling to match design

--- header.php
+++ header.php
@@ -16,6 +16,11 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
 
 	<?php wp_head(); ?>
 </head>
@@ -28,7 +33,7 @@
 		<?php if ( is_page_template( 'homepage.php' ) ) : ?>
 			<div class="homepage-topbar">
 				<div class="homepage-topbar__inner">
-					<p>Call us now! <strong>385.154.11.28.35</strong></p>
+					<p><span>Call us now!</span><strong>385.154.11.28.35</strong></p>
 					<div class="homepage-topbar__links">
 						<a href="#">Login</a>
 						<a href="#">Signup</a>
--- homepage.php
+++ homepage.php
@@ -15,7 +15,7 @@ get_header();
             <strong>Contact</strong>
         </p>
 
-        <h1 class="homepage-mockup__title"><?php the_title(); ?></h1>
+        <h1 class="homepage-mockup__title">Contact</h1>
         <p class="homepage-mockup__intro">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam posuere ipsum nec velit mattis elementum.
             Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas eu placerat
@@ -47,9 +47,11 @@ get_header();
             <div class="homepage-mockup__column homepage-mockup__column--info">
                 <h2>Reach Us</h2>
                 <div class="homepage-mockup__address">
-                    <p><strong>Coalition Skills Test</strong></p>
-                    <p>535 La Plata Street</p>
-                    <p>4200 Argentina</p>
+                    <p>Coalition Skills Test</p>
+                    <p class="homepage-mockup__address-info">
+                        535 La Plata Street<br>
+                        4200 Argentina
+                    </p>
                     <p class="homepage-mockup__contact-lines">
                         Phone: 385.154.11.28.38<br>
                         Fax: 385.154.35.66.78
@@ -57,10 +59,34 @@ get_header();
                 </div>
 
                 <ul class="homepage-mockup__social" aria-label="Social links">
-                    <li><a href="#" aria-label="Facebook">f</a></li>
-                    <li><a href="#" aria-label="Twitter">t</a></li>
-                    <li><a href="#" aria-label="LinkedIn">in</a></li>
-                    <li><a href="#" aria-label="Pinterest">p</a></li>
+                    <li>
+                        <a href="#" aria-label="Facebook">
+                            <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+                                <path d="M13.5 8H16V5h-2.5c-2.2 0-3.5 1.5-3.5 3.8V11H8v3h2v5h3v-5h2.5l.5-3H13v-2c0-.7.3-1 1-1z"></path>
+                            </svg>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" aria-label="Twitter">
+                            <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+                                <path d="M22 6.2c-.7.3-1.4.5-2.1.6.8-.5 1.3-1.2 1.6-2.1-.7.4-1.6.8-2.4.9A3.8 3.8 0 0 0 12.5 9c0 .3 0 .6.1.9-3.1-.2-5.8-1.7-7.7-4-.3.5-.5 1.2-.5 1.8 0 1.3.6 2.4 1.7 3.1-.6 0-1.2-.2-1.7-.5v.1c0 1.8 1.3 3.3 3 3.6-.3.1-.7.2-1 .2-.2 0-.5 0-.7-.1.5 1.5 2 2.6 3.7 2.7A7.7 7.7 0 0 1 4 18.5 10.8 10.8 0 0 0 9.9 20c7 0 10.8-5.8 10.8-10.8v-.5c.8-.5 1.4-1.2 1.9-2z"></path>
+                            </svg>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" aria-label="LinkedIn">
+                            <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+                                <path d="M6.4 8.8a1.9 1.9 0 1 1 0-3.8 1.9 1.9 0 0 1 0 3.8zM4.8 10h3.2v9.2H4.8V10zm5.2 0h3v1.3h.1c.4-.8 1.5-1.6 3.1-1.6 3.3 0 3.9 2.1 3.9 4.9v4.6h-3.2v-4.1c0-1 0-2.2-1.4-2.2s-1.6 1-1.6 2.1v4.2H10V10z"></path>
+                            </svg>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" aria-label="Pinterest">
+                            <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+                                <path d="M12 4C7.8 4 5 6.9 5 10.8c0 3 1.7 4.7 2.7 4.7.4 0 .7-1 .7-1.3 0-.3-.8-1-1-2.5-.2-2.9 2.2-5.5 5.3-5.5 2.9 0 4.9 1.6 4.9 4.6 0 2.2-.9 6.4-3.9 6.4-1.1 0-2-1-1.7-2.1.3-1.3.8-2.7.8-4.1 0-2.4-3.4-2.2-3.4 1.1 0 1 .3 1.6.3 1.6l-1.3 5.6c1.2.4 2.4.6 3.7.6 4.8 0 8.2-3.5 8.2-8.3C20.3 7.3 16.8 4 12 4z"></path>
+                            </svg>
+                        </a>
+                    </li>
                 </ul>
             </div>
         </div>
